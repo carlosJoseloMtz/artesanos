65049c0f91d0eca3683f0c79606eaf5fe53b5c68
Tomas <matuser00@gmail.com>
2020-05-16 00:34:43 -0500

    Se omite seleccion de texto de boton

--- frontend/artesanos-app/src/components/NopalButton.vue
+++ frontend/artesanos-app/src/components/NopalButton.vue
@@ -16,6 +16,16 @@ export default {
 @import '../styles/_variables.scss';
 @import 'material-icons/css/material-icons.scss';
 
+@mixin avoid_textselection($args...) {
+    -webkit-touch-callout: $args; /* iOS Safari */
+    -webkit-user-select: $args; /* Safari */
+     -khtml-user-select: $args; /* Konqueror HTML */
+       -moz-user-select: $args; /* Firefox */
+        -ms-user-select: $args; /* Internet Explorer/Edge */
+            user-select: $args; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+  }
+
 @mixin pushed_transition($args...) {
     -webkit-transition: $args;
     -moz-transition: $args;
@@ -26,7 +36,7 @@ export default {
 
 button {
   @include pushed_transition(background-color 0.4s ease);
-
+  @include avoid_textselection(none);
   background-color: $primary-color;
   border: none;
   border-radius: 4.5px;
